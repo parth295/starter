1d0eaced7675fda06acbd638568daf83871935f3
parth295 <parth295@gmail.com>
2020-09-19 13:29:41 -0700

    commit

--- app.js
+++ app.js
@@ -13,23 +13,47 @@ var scores, roundScore, activePlayer, dice;
 scores=[0,0];
 roundScore = 0 ;
 activePlayer=0;
+document.querySelector('.dice').style.display='none';
+document.getElementById('score-0').textContent='0';
+document.getElementById('score-1').textContent='0';
+document.getElementById('current-0').textContent='0';
+document.getElementById('current-1').textContent='0';
 
+document.querySelector('.btn-roll').addEventListener('click', function(){
 
-dice=Math.floor(Math.random()*6)+1;
+//1. Random number
+    var dice=Math.floor(Math.random()*6)+1;
+//2. Display the result
+    var diceDOM=document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src= 'dice-' + dice + '.png';
+//3. update the round score if the rolled number was not 1
 
-document.querySelector('#current-'+activePlayer).textContent=dice;
+    if(dice!==1){
+        //add score
+        roundScore+=dice;
+        document.querySelector('#current-'+ activePlayer).textContent=roundScore;
+        }
+    else{
+        //next player
+        activePlayer===0? activePlayer =1 : activePlayer=0;
+        roundScore=0;
+        document.getElementById('current-0').textContent='0';
+        document.getElementById('current-1').textContent='0';
 
-var x= document.querySelector('#score-0').textContent;
+        document.querySelector('.player-0-panel').classList.remove('active');
+        document.querySelector('.player-1-panel').classList.add('active');
+        
 
-document.querySelector('.dice').style.display='dice-'+dice ;
+    }
+});
 
-cdkmdd
+//
+//document.querySelector('#current-'+activePlayer).textContent=dice;
 
+//var x= document.querySelector('#score-0').textContent;
 
-
-
-
-
+//document.querySelector('.dice').style.display='none';
 
 
 
